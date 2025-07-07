71b21dec92d4ea3c4ebcc1c42aa62b614a927333
penguinawesome1 <owen.h.colley@gmail.com>
2025-07-06 21:40:44 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,6 @@ mod world;
 pub use crate::{
     block::{ Block },
     chunk::{ Chunk, CHUNK_WIDTH, CHUNK_HEIGHT, CHUNK_DEPTH, CHUNK_VOLUME },
-    coords::{ ChunkPosition, BlockPosition, chunk_to_block_pos },
-    world::{ World, BlockAccessError, global_coords_in_chunks },
+    coords::{ ChunkPosition, BlockPosition, chunk_to_block_pos, block_to_chunk_pos, block_offsets },
+    world::{ World, BlockAccessError, positions_in_square, global_coords_in_chunks },
 };
